2fe4f16cd3fcc338f1df5b1aa5ad5bee35e78de6
rabab003 <bintaliraq4@gmail.com>
2025-02-28 16:57:25 +0300

    4:57

---
+++ src/Posts.jsx
@@ -0,0 +1,15 @@
+export default function Posts(){
+    return(
+        <div style={{
+            padding:'10px',
+            border:"solid teal 5px",
+            margin:"25px"
+        }}>
+            <h2>this is the post title</h2>
+            <hr />
+            <p>this is the post body</p>
+
+        </div>
+    )
+
+}
